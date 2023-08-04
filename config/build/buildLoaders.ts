6e7ff72242b477484adf0d4c73e7c4f78d6e0b1e
Julia <yulia.bobko@mail.ru>
2023-08-04 12:36:42 +0300

    add module scss

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,15 +2,22 @@ import webpack from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/config";
 
-export function buildLoaders(options:BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({isDev}:BuildOptions): webpack.RuleSetRule[] {
 
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
             // Creates `style` nodes from JS strings
-            options.isDev ? 'style-loader': MiniCssExtractPlugin.loader,
+            isDev ? 'style-loader': MiniCssExtractPlugin.loader,
             // Translates CSS into CommonJS
-            "css-loader",
+            {
+                loader: "css-loader",
+                options:{
+                    modules:true,
+                    auto:(resPath:string)=>Boolean(resPath.includes('.module.')),
+                    //localIdentName: isDev ?'[path][name]__[local]:['hash']
+                }
+            },
             // Compiles Sass to CSS
             "sass-loader",
         ],
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,10 +1,10 @@
 import React, {useState} from 'react';
-import './Counter.scss'
+import classes from './Counter.module.scss'
 
 export const Counter = () => {
     const [count, setCount] = useState(0)
     return (
-        <div>
+        <div className={classes.btn}>
             {count}
             <button onClick={()=>setCount(prev => prev+1)}>increment</button>
         </div>
