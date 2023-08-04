72ca4c0af27986bf51886e7e2b6a8ed515033092
Julia <yulia.bobko@mail.ru>
2023-08-04 14:05:07 +0300

    routing AboutPage Main Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import {Link, Route, Routes} from "react-router-dom";
 import {Counter} from "./components/Counter";
 import './index.scss'
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
 
 export const App = () => {
     return <>
         <div className={'app'}>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Routes>
+                <Route path={'/about'} element={<AboutPage/>}/>
+                <Route path={'/'} element={<MainPage/>}/>
+            </Routes>
             hello
         </div>
         <Counter/>
