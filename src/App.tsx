7469ec15334f2d3815cfeea0c2759a81a181588c
Julia <yulia.bobko@mail.ru>
2023-08-04 15:38:14 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React, {Suspense} from "react";
 import {Link, Route, Routes} from "react-router-dom";
 import {Counter} from "./components/Counter";
-import './index.scss'
+import './styles/index.scss'
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
 export const App = () => {
     return <>
-        <div className={'app'}>
+        <div className={'app dark'}>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading.....</div>}><Routes>
