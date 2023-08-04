c44e2c28f67d90aa85b5f2e96abb783df523fe82
Julia <yulia.bobko@mail.ru>
2023-08-04 16:55:20 +0300

    app with ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@ import {Counter} from "./components/Counter";
 import './styles/index.scss'
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
+
 
 export const App = () => {
+    const {theme, toggleTheme} = useTheme()
     return <>
-        <div className={'app dark'}>
+        <button onClick={toggleTheme}>toggle theme</button>
+        <div className={`app ${theme}`}>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading.....</div>}><Routes>
