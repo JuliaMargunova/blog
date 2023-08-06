bd6ffa0f65bcaedde6a88eac2930460549b196ea
Julia <yulia.bobko@mail.ru>
2023-08-06 23:09:48 +0300

    add routeConfig

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -2,13 +2,19 @@ import React, {Suspense} from 'react';
 import {Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "pages/MainPage/MainPage.async";
+import {routeConfig} from "shared/config/routeConfig/routeConfig";
 
 const AppRouter = () => {
     return (
         <Suspense fallback={<div>Loading.....</div>}>
             <Routes>
-                <Route path={'/about'} element={<AboutPageAsync/>}/>
-                <Route path={'/'} element={<MainPageAsync/>}/>
+                {Object.values(routeConfig).map(({path,element})=>(
+                    <Route
+                        key = {path}
+                        element={element}
+                        path={path}
+                    />
+                ))}
             </Routes>
         </Suspense>
     );
