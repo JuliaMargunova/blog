2098ee70da833a6fd0d56e5660b3dc9082cdd681
Julia <yulia.bobko@mail.ru>
2023-08-12 19:48:01 +0300

    add regenerator-runtime, add tests for btn, sidebar

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -7,9 +7,9 @@ describe('Button', () => {
         expect(screen.getByText('TEST')).toBeInTheDocument();
     });
 
-    // test('Test clear theme', () => {
-    //     render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
-    //     expect(screen.getByText('TEST')).toHaveClass('clear');
-    //     screen.debug();
-    // });
+    test('Test clear theme', () => {
+        render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
+        expect(screen.getByText('TEST')).toHaveClass('clear');
+        screen.debug();
+    });
 });
