c6422ed6fbe9b0999bd01a7c24494305e45776ad
Julia <yulia.bobko@mail.ru>
2023-08-19 12:54:51 +0300

    add profile, optimization render

---
+++ src/shared/const/common.ts
@@ -0,0 +1,13 @@
+export enum Currency{
+    "RUB"="RUB",
+    "EUS"="EUS",
+    "USD"="USD"
+}
+
+export enum Country {
+    "Russia"="Russia",
+    "Belarus"="Belarus",
+    "Ukraine"="Ukraine",
+    "Kazakhstan"="Kazakhstan",
+    "Armenia"="Armenia",
+}
--- src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -1,38 +1,42 @@
-import React, { FC, useEffect } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { FC, useEffect } from 'react';
+import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
 import { useDispatch, useStore } from 'react-redux';
-import { ReduxStoreWithManager } from 'app/providers/StoreProvider';
-import { StateSchemaKey } from 'app/providers/StoreProvider/config/StateSchema';
+import { ReduxStoreWithManager, StateSchemaKey } from 'app/providers/StoreProvider/config/StateSchema';
 import { Reducer } from '@reduxjs/toolkit';
+import cls from './DynamicModuleLoader.module.scss';
 
-export type ReducersList ={
-    [name in StateSchemaKey]?:Reducer;
+export type ReducersList = {
+    [name in StateSchemaKey]?: Reducer;
 }
 
-type ReducersListEntry = [StateSchemaKey, Reducer];
+type ReducersListEntry = [StateSchemaKey, Reducer]
 
 interface DynamicModuleLoaderProps {
     reducers: ReducersList;
     removeAfterUnmount?: boolean;
 }
 
-export const DynamicModuleLoader:FC<DynamicModuleLoaderProps> = (props) => {
+export const DynamicModuleLoader: FC<DynamicModuleLoaderProps> = (props) => {
     const {
         children,
         reducers,
         removeAfterUnmount,
     } = props;
+
     const store = useStore() as ReduxStoreWithManager;
     const dispatch = useDispatch();
 
     useEffect(() => {
-        Object.entries(reducers).forEach(([name, reducer]:ReducersListEntry) => {
+        Object.entries(reducers).forEach(([name, reducer]: ReducersListEntry) => {
             store.reducerManager.add(name, reducer);
             dispatch({ type: `@INIT ${name} reducer` });
         });
 
         return () => {
-            if (!removeAfterUnmount) {
-                Object.entries(reducers).forEach(([name, reducer]:ReducersListEntry) => {
+            if (removeAfterUnmount) {
+                Object.entries(reducers).forEach(([name, reducer]: ReducersListEntry) => {
                     store.reducerManager.remove(name);
                     dispatch({ type: `@DESTROY ${name} reducer` });
                 });
@@ -40,6 +44,7 @@ export const DynamicModuleLoader:FC<DynamicModuleLoaderProps> = (props) => {
         };
         // eslint-disable-next-line
     }, []);
+
     return (
         // eslint-disable-next-line react/jsx-no-useless-fragment
         <>
