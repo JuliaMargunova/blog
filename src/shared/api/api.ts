6f8599188818d7a4168f79bb4b6edb38e14fe554
Julia <yulia.bobko@mail.ru>
2023-08-20 12:27:12 +0300

    add global variable __API__, add instance axios, add thunk extraArgument

---
+++ src/shared/api/api.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
+
+export const $api = axios.create({
+    baseURL: __API__,
+    headers: {
+        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
+    },
+});
+
+// const baseUrl = __IS_DEV__ ? 'http://localhost:8000' : 'http://production.ru';
+// export const $api = axios.create({
+//     baseURL: baseUrl,
+//     headers: {
+//         authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY),
+//     },
+// });
