3f6b330bba9fab4703aa0062e138bda6d88815ef
Julia <yulia.bobko@mail.ru>
2023-09-05 01:17:35 +0300

    add article blocks, add skeleton, thunk, slice

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,15 +1,29 @@
 import React, { memo } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { ArticleDetails } from 'entities/Article';
+import { useParams } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import cls from './ArticleDetailsPage.module.scss';
 
 interface ArticleDetailsPageProps {
     className?: string;
 }
 
-const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => (
-    <div className={classNames(cls.ArcticleDetailsPage, {}, [className])}>
-        Article Detail Page
-    </div>
-);
+const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
+    const { id } = useParams<{id:string}>();
+    const { t } = useTranslation('article_details');
+    if (!id) {
+        return (
+            <div className={classNames(cls.ArcticleDetailsPage, {}, [className])}>
+                {t('Статья не найдена')}
+            </div>
+        );
+    }
+    return (
+        <div className={classNames(cls.ArcticleDetailsPage, {}, [className])}>
+            <ArticleDetails id={id} />
+        </div>
+    );
+};
 
 export default memo(ArticleDetailsPage);
