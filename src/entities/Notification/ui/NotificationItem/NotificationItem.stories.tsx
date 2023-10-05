ea7dfb0c3997de31de896501589047d3e8478399
Julia <yulia.bobko@mail.ru>
2023-10-05 15:46:13 +0300

    refactor: add notification stories

--- src/entities/Notification/ui/NotificationItem/NotificationItem.stories.tsx
+++ src/entities/Notification/ui/NotificationItem/NotificationItem.stories.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
+import withMock from 'storybook-addon-mock';
 import { NotificationItem } from './NotificationItem';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 
 export default {
-    title: 'shared/NotificationItem',
+    title: 'entities/Notification/NotificationItem',
     component: NotificationItem,
     argTypes: {
         backgroundColor: { control: 'color' },
     },
+    decorators: [withMock],
 } as ComponentMeta<typeof NotificationItem>;
 
 const Template: ComponentStory<typeof NotificationItem> = (args) => <NotificationItem {...args} />;
 
 export const Normal = Template.bind({});
-Normal.args = {};
+Normal.args = {
+    item: {
+        id: '1',
+        title: 'уведомление',
+        description: 'оцени пользователя',
+    },
+};
+Normal.decorators = [StoreDecorator({})];
