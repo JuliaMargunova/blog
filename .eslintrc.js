0096d316e6e81d1478c1ab6981511851046cd70d
Julia <yulia.bobko@mail.ru>
2023-10-18 11:24:29 +0300

    redesign: sidebar implemented new font and icons, modified layout of the homepage, updated button styles and color scheme

--- .eslintrc.js
+++ .eslintrc.js
@@ -60,6 +60,8 @@ module.exports = {
                     'justify',
                     'border',
                     'feature',
+                    'color',
+                    'variant',
                 ],
             },
         ],
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,15 +1,17 @@
 import { memo, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Button, ButtonSize, ButtonTheme } from '@/shared/ui/deprecated/Button';
-import { VStack } from '@/shared/ui/deprecated/Stack';
-import { AppLogo } from '@/shared/ui/deprecated/AppLogo';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { getSidebarItems } from '../../model/selectors/getSidebarItems';
-import cls from './Sidebar.module.scss';
-import { SidebarItem } from '../SidebarItem/SidebarItem';
 import { ThemeSwitcher } from '@/features/ThemeSwitcher';
 import { LangSwitcher } from '@/features/LangSwitcher';
+import { Button, ButtonSize, ButtonTheme } from '@/shared/ui/deprecated/Button';
+import { VStack } from '@/shared/ui/deprecated/Stack';
+import cls from './Sidebar.module.scss';
+import { SidebarItem } from '../SidebarItem/SidebarItem';
+import { getSidebarItems } from '../../model/selectors/getSidebarItems';
 import { ToggleFeatures } from '@/shared/lib/features';
+import { AppLogo } from '@/shared/ui/redesigned/AppLogo';
+import { Icon } from '@/shared/ui/redesigned/Icon';
+import ArrowIcon from '@/shared/assets/icons/arrow-bottom.svg';
 
 interface SidebarProps {
     className?: string;
@@ -18,7 +20,9 @@ interface SidebarProps {
 export const Sidebar = memo(({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
     const sidebarItemsList = useSelector(getSidebarItems);
+
     const onToggle = () => {
+        console.log(collapsed);
         setCollapsed((prev) => !prev);
     };
 
@@ -42,23 +46,24 @@ export const Sidebar = memo(({ className }: SidebarProps) => {
                     data-testid="sidebar"
                     className={classNames(
                         cls.SidebarRedesigned,
-                        { [cls.collapsed]: collapsed },
+                        { [cls.collapsedRedesigned]: collapsed },
                         [className],
                     )}
                 >
-                    <AppLogo className={cls.appLogo} />
-                    <Button
-                        data-testid="sidebar-toggle"
-                        onClick={onToggle}
-                        className={cls.collapseBtn}
-                        theme={ButtonTheme.BACKGROUND_INVERTED}
-                        size={ButtonSize.L}
-                    >
-                        {collapsed ? '>' : '<'}
-                    </Button>
+                    <AppLogo
+                        size={collapsed ? 30 : 50}
+                        className={cls.appLogo}
+                    />
                     <VStack role="navigation" gap="8" className={cls.items}>
                         {itemsList}
                     </VStack>
+                    <Icon
+                        data-testid="sidebar-toggle"
+                        onClick={onToggle}
+                        className={cls.collapseBtn}
+                        Svg={ArrowIcon}
+                        clickable
+                    />
                     <div className={cls.switchers}>
                         <ThemeSwitcher />
                         <LangSwitcher short={collapsed} className={cls.lang} />
@@ -80,6 +85,7 @@ export const Sidebar = memo(({ className }: SidebarProps) => {
                         className={cls.collapseBtn}
                         theme={ButtonTheme.BACKGROUND_INVERTED}
                         size={ButtonSize.L}
+                        square
                     >
                         {collapsed ? '>' : '<'}
                     </Button>
