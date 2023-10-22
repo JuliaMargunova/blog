e34084c7ebd68266dcee980463bd0de3528a5c4b
Julia <yulia.bobko@mail.ru>
2023-10-22 23:15:07 +0300

    redesign: comments, rating card

--- src/shared/ui/redesigned/Modal/Modal.tsx
+++ src/shared/ui/redesigned/Modal/Modal.tsx
@@ -1,9 +1,9 @@
 import React, { ReactNode } from 'react';
+import { classNames, Mods } from '@/shared/lib/classNames/classNames';
+import { useModal } from '@/shared/lib/hooks/useModal/useModal';
 import { Overlay } from '../Overlay/Overlay';
 import { Portal } from '../Portal/Portal';
 import cls from './Modal.module.scss';
-import { useModal } from '@/shared/lib/hooks/useModal/useModal';
-import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
 import { toggleFeatures } from '@/shared/lib/features';
 
@@ -11,7 +11,6 @@ interface ModalProps {
     className?: string;
     children?: ReactNode;
     isOpen?: boolean;
-
     onClose?: () => void;
     lazy?: boolean;
 }
